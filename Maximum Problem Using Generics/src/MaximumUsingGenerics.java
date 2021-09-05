91da3eb6e53990da0549712c81d3898a34f2d5d2
Sajan <sajanmhatre29@gmail.com>
2021-09-05 17:41:04 +0530

    Maximum Integer

--- Maximum Problem Using Generics/src/MaximumUsingGenerics.java
+++ Maximum Problem Using Generics/src/MaximumUsingGenerics.java
@@ -1,32 +1,39 @@
-public class MaximumUsingGenerics {
-    //instance variables
-    int x;
-    int y;
-    int z;
-    //Constructor to get the values of x,y,z
-    public MaximumUsingGenerics(int x, int y, int z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-    //Method to find Maximum of 3 numbers
-    void maximum(){
-        int max =  Math.max(Math.max(Integer.valueOf(x),Integer.valueOf(y)),Integer.valueOf(z));
-        System.out.println("Maximum of 3 Numbers is: "+max);
+    public class MaximumUsingGenerics {
+        //instance Vairiables
+         Integer x;
+         Integer y;
+         Integer z;
+        //Constructor
+        public MaximumUsingGenerics(Integer x, Integer y, Integer z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        //Method to get the Maximum of Integers
+        Integer maxTest() {
+            Integer max = x;
+            if (y.compareTo(max) > 0)
+                max = y;
+            {
+                if (z.compareTo(max) > 0) {
+                    max = z;//z is largest now
+                }
+                System.out.println("Maximum of the Integers is: "+max);
+                return max; //returns the largest object
+            }
+
+        }
+        public static void main(String[] args) {
+            System.out.println("Welcome to find the Maximum Integer...");
+            //Created an Object
+            MaximumUsingGenerics mx = new MaximumUsingGenerics(988,567,999);
+            mx.maxTest();
+
+
+        }
     }
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to find Maximum using Generics");
-        //Create an Object
-        MaximumUsingGenerics obj = new MaximumUsingGenerics(120,545,320);
-//        System.out.println(Integer.valueOf(obj.x));
-//        System.out.println(Integer.valueOf(obj.y));
-//        System.out.println(Integer.valueOf(obj.z));
-        
-        //Call the Method to find Maximum of 3 Numbers
-        obj.maximum();
-//        int max =  Math.max(Math.max(Integer.valueOf(obj.x),Integer.valueOf(obj.y)),Integer.valueOf(obj.z));
-//        System.out.println("Maximum of 3 NUmbers is: "+max);
-    }
 
-}
+
+
+
