b3f4f6a0802df69e9d3babb292312b55c72b3bb3
Sajan <sajanmhatre29@gmail.com>
2021-09-05 19:26:58 +0530

    Generic Maximum

--- Maximum Problem Using Generics/src/MaximumUsingGenerics.java
+++ Maximum Problem Using Generics/src/MaximumUsingGenerics.java
@@ -1,5 +1,38 @@
-public class MaximumUsingGenerics {
+public class MaximumUsingGenerics <T extends Comparable> {
+    //instance Vairiables
+     T x;
+     T y;
+     T z;
+    //Constructor
+    public MaximumUsingGenerics(T x, T y, T z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    //Method to get the Maximum of Integers
+   T maxTest() {
+        T max = x;
+        if (y.compareTo(max) > 0)
+            max = y;
+        {
+            if (z.compareTo(max) > 0) {
+                max = z;//z is largest now
+            }
+            System.out.println("Maximum  is: "+max);
+            return max; //returns the largest object
+        }
+
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to find Maximum using Generics");
+        System.out.println("Welcome to find the Maximum ...");
+        //Created an Object
+        MaximumUsingGenerics integer = new MaximumUsingGenerics(343,567,124);
+        MaximumUsingGenerics flt = new MaximumUsingGenerics(23.45,78.45,97.12);
+        MaximumUsingGenerics str = new MaximumUsingGenerics("Apple","Peach","Banana");
+        integer.maxTest();
+        flt.maxTest();
+        str.maxTest();
+
+
     }
 }
